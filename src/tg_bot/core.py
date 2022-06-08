ce5513de9012a04d47fb813b20f279626a6238b6
moonburnt <moonburnt@disroot.org>
2022-06-08 18:50:30 +0300

    switch from requests to aiohttp; fix formatting
    - Switch from requests-powered calls to api to aiohttp. Now bot should
    be fully asynchronous (I've originally picked requests coz I've planned
    to parse yandex weather, but then I've ditched it in favor of different
    api. So now there is no point to artifically restrict amount of calls).
    - Format sources with black

--- src/tg_bot/core.py
+++ src/tg_bot/core.py
@@ -1,11 +1,8 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils import markdown
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiohttp import client_exceptions
+from aiohttp import client_exceptions, ClientSession
 from asyncio import gather
-
-from requests import Session
-
 from sys import exit
 import logging
 
@@ -28,7 +25,7 @@ async def on_shutdown(dispatcher: Dispatcher):
 
 
 class WeatherBot(Bot):
-    def __init__(self, token: str, storage = None):
+    def __init__(self, token: str, storage=None):
         super().__init__(token=token)
 
         if storage is None:
@@ -36,10 +33,10 @@ class WeatherBot(Bot):
 
         self.dp = Dispatcher(self, storage=storage)
 
-        self.fetcher_session = Session()
-        self.fetcher_session.headers["user-agent"] = (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"
-        )
+        self.fetcher_session = ClientSession()
+        self.fetcher_session.headers[
+            "user-agent"
+        ] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"
 
         self.known_weather = {}
 
@@ -50,6 +47,7 @@ class WeatherBot(Bot):
             on_shutdown=on_shutdown,
             skip_updates=True,
         )
+
 
 def make_bot(token: str) -> WeatherBot:
     bot = WeatherBot(
@@ -75,17 +73,19 @@ def make_bot(token: str) -> WeatherBot:
 
         request = message.text.split("/weather")[1]
 
-        answ = bot.fetcher_session.get(f"https://www.wttr.in/{request}?format=4")
-
         txt = ""
-        match answ.status_code:
-            case 200:
-                txt = answ.text
-            case 404:
-                txt = "Unknown location, please try again"
-            case _:
-                txt = "An error occured, please try different search"
-                log.warning(f"Weather api returned {answ.status_code}")
+
+        async with bot.fetcher_session.get(
+            f"https://www.wttr.in/{request}?format=4"
+        ) as answ:
+            match answ.status:
+                case 200:
+                    txt = await answ.text()
+                case 404:
+                    txt = "Unknown location, please try again"
+                case _:
+                    txt = "An error occured, please try different search"
+                    log.warning(f"Weather api returned {answ.status_code}")
 
         await message.reply(txt)
 
